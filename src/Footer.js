dce2def2ead8e7c5a7dd18602784378d9ffec977
imanfsx <iman30039581@gmail.com>
2024-05-29 22:37:27 +1000

    update to footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,23 +4,15 @@ export default function Footer() {
   return (
     <p>
       This project was coded by{" "}
-      <a href="https://github.com/jmisshela" target="_blank" rel="noreferrer">
-        Jane Misshela
+      <a href="https://github.com/imanfsx" target="_blank" rel="noreferrer">
+        IMAN SOHAIL
       </a>
       , is open-sourced on{" "}
-      <a
-        href="https://github.com/jmisshela/weather-react-week4"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href="" target="_blank" rel="noreferrer">
         GitHub
       </a>{" "}
       and hosted on{" "}
-      <a
-        href="https://weather-react-week4-jane.netlify.app"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href="" target="_blank" rel="noreferrer">
         Netlify
       </a>
     </p>
